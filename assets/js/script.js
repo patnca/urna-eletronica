4ee4f101a61fd79f41b5a524530ad2fb80abe01b
Patrick França <psfranca@gmail.com>
2022-02-21 21:08:11 -0300

    Inserindo as funções

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,36 +14,78 @@ let numero = '';
 let votoBranco = false;
 let votos = [];
 
-botao[0].addEventListener('click', () => {
-    alert('clique no 1');
-});
-botao[1].addEventListener('click', () => {
-    alert('clique no 2');
-});
-botao[2].addEventListener('click', () => {
-    alert('clique no 3');
-});
-botao[3].addEventListener('click', () => {
-    alert('clique no 4');
-});
-botao[4].addEventListener('click', () => {
-    alert('clique no 5');
-});
-botao[5].addEventListener('click', () => {
-    alert('clique no 6');
-});
-botao[6].addEventListener('click', () => {
-    alert('clique no 7');
-});
-botao[7].addEventListener('click', () => {
-    alert('clique no 8');
-});
-botao[8].addEventListener('click', () => {
-    alert('clique no 9');
-});
-botao[9].addEventListener('click', () => {
-    alert('clique no 0');
-});
+function iniciaVotacao() {
+    let etapa = etapas[etapaAtual];
+    let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
+    
+    for(let i=0; i < etapa.numeros; i++){
+        if(i===0){
+            numeroHtml += '<div class="tela__num blink"></div>';
+        } else {
+            numeroHtml += '<div class="tela__num"></div>';
+        }
+    }
+
+    titulo.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    dados.innerHTML = '';
+    rodape.style.display = 'none';
+    imagens.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+        titulo.style.display = 'block';
+        rodape.style.display = 'block';
+        dados.innerHTML = `Nome: ${candidato.nome}<br>Partido: ${candidato.partido}`;
+        let fotosHtml = '';
+        for( let i in candidato.fotos) {
+            if(candidato.fotos[i].small) {
+                fotosHtml += `<div class="tela__image tela__image--small"><img src="assets/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            } else {
+                fotosHtml += `<div class="tela__image"><img src="assets/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
+            }
+        }
+        imagens.innerHTML = fotosHtml;
+    } else {
+        titulo.style.display = 'block';
+        rodape.style.display = 'block'
+        dados.innerHTML = `<div class"aviso--grande blink">VOTO NULO</div>`;
+    }
+}
+
+
+botao.forEach(button => {
+    button.addEventListener('click', () => {
+        let num = document.querySelector('.tela__num.blink');
+        const n = button.dataset.number
+
+        if (num !== null) {
+            num.innerHTML = n;
+            numero = `${numero}${n}`;
+
+            num.classList.remove('blink');
+            if(num.nextElementSibling !== null) {
+                num.nextElementSibling.classList.add('blink');
+            } else {
+                atualizaInterface()
+            }
+        }
+    })
+})
+
 
 branco.addEventListener('click', () => {
     numero = '';
@@ -82,64 +124,10 @@ confirma.addEventListener('click', () => {
         if(etapas[etapaAtual] !== undefined) {
             iniciaVotacao();
         } else {
-            document.querySelector('.tela').innerHTML = '<div class="aviso--maior blink">FIM</div>';
+            document.querySelector('.tela').innerHTML = '<div class="aviso--fim blink">FIM</div>';
             console.log(votos)
         }
     }
 });
-
-function iniciaVotacao() {
-    let etapa = etapas[etapaAtual];
-    let numeroHtml = '';
-    numero = '';
-    votoBranco = false;
-
-    for(let i=0; i < etapa.numeros; i++){
-        if(i===0){
-            numeroHtml += '<div class="tela__num blink"></div>';
-        } else {
-            numeroHtml += '<div class="numero"></div>';
-        }
-    }
-
-    titulo.style.display = 'none';
-    cargo.innerHTML = etapa.titulo;
-    dados.innerHTML = '';
-    rodape.style.display = 'none';
-    imagens.innerHTML = '';
-    numeros.innerHTML = numeroHtml;
-}
-
-function atualizaInterface() {
-    let etapa = etapas[etapaAtual];
-    let candidato = etapa.candidatos.filter((item)=>{
-        if(item.numero === numero) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-    if(candidato.length > 0) {
-        candidato = candidato[0];
-        titulo.style.display = 'block';
-        rodape.style.display = 'block';
-        dados.innerHTML = `Nome: ${candidato.nome}<br>Partido: ${candidato.partido}`;
-        let fotosHtml = '';
-        for( let i in candidato.fotos) {
-            if(candidato.fotos[i].small) {
-                fotosHtml += `<div class="tela__image tela__imagem--small"<img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
-            } else {
-                fotosHtml += `<div class="tela__image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
-            }
-        }
-        imagens.innerHTML = fotosHtml;
-    } else {
-        titulo.style.display = 'block';
-        rodape.style.display = 'block'
-        dados.innerHTML = `<div class"aviso--grande blink">VOTO NULO</div>`;
-    }
-}
-
-
 
 iniciaVotacao();
